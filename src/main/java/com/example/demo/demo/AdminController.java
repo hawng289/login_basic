70efcbceecf910a921e6d75aa177dc3354498d70
sqrt(1-cos**2) <114472721+hawng289@users.noreply.github.com>
2023-11-20 08:17:01 +0700

    .

---
+++ src/main/java/com/example/demo/demo/AdminController.java
@@ -0,0 +1,48 @@
+package com.example.demo.demo;
+
+import com.example.demo.config.ApplicationConfig;
+import com.example.demo.user.User;
+import com.example.demo.user.UserRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/admin")
+@PreAuthorize("hasRole('ADMIN')")
+public class AdminController {
+    ApplicationConfig applicationConfig;
+    private UserRepository userRepository;
+
+    @GetMapping
+    @PreAuthorize("hasAuthority('admin:read')")
+    public String get() {
+        return "GET:: admin controller";
+    }
+    @PostMapping
+    @PreAuthorize("hasAuthority('admin:create')")
+
+    public String post(@RequestBody User userDTO) {
+
+
+            userDTO.setPassword(applicationConfig.passwordEncoder().encode(userDTO.getPassword()));
+            // Thiết lập các thông tin khác của người dùng
+            // ...
+
+            userRepository.save(userDTO); // Lưu người dùng vào cơ sở dữ liệu
+
+            return "User created successfully";
+    }
+    @PutMapping
+    @PreAuthorize("hasAuthority('admin:update')")
+
+    public String put() {
+        return "PUT:: admin controller";
+    }
+    @DeleteMapping
+    @PreAuthorize("hasAuthority('admin:delete')")
+
+    public String delete() {
+        return "DELETE:: admin controller";
+    }
+}
---
+++ src/main/java/com/example/demo/demo/ManagementController.java
@@ -0,0 +1,34 @@
+package com.example.demo.demo;
+
+
+import io.swagger.oas.annotations.Operation;
+import io.swagger.oas.annotations.responses.ApiResponse;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/management")
+
+public class ManagementController {
+
+    @GetMapping("/get")
+    public String get() {
+        return "GET:: management controller";
+    }
+    @PostMapping("/post")
+    public String post() {
+        return "POST:: management controller";
+    }
+    @PutMapping("/put")
+    public String put() {
+        return "PUT:: management controller";
+    }
+    @DeleteMapping("/delete")
+    public String delete() {
+        return "DELETE:: management controller";
+    }
+}
